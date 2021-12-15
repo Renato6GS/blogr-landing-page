629819f71672fdac5bf645123b4b54ba34221a3f
Renatogranados <renatogranadosgt@gmail.com>
2021-12-14 21:19:37 -0600

    removing comments

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
-// You can add a list of files
 function importAll(r) {
    return r.keys().map(r);
 }
 importAll(require.context('./static/img', false, /\.(png|jpe?g|svg)$/));
 
-// ... or, add files individually
 import './vendors/normalize/normalize.css';
 import './styles/main.css';
 import './styles/header.css';
@@ -70,5 +68,3 @@ document.addEventListener('click', (e) => {
       menuOpen = false;
    }
 });
-
-// Nos quedamos programando la rotación de la flechita
